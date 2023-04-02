2e4d33b58a3d91c27a7cb22f4ce9c4ee3901ac5e
Shashwatkkkk <128404937+Shashwatkkkk@users.noreply.github.com>
2023-04-02 12:57:02 +0530

    An exciting game

---
+++ Treasue_Island.py
@@ -0,0 +1,44 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+choice1 = input('You are at a crossroad, where do you want to go? Type "right" or "left".').lower()
+if choice1== "right":
+    print("You got hit by the truck")
+else:
+    choice2 = input('You have come to a lake, there is an island in the middle of the lake.Type "wait" to wait for a boat. Type "swim" to swim across').lower
+    if choice2 == "swim":
+        print("A shark ate you")
+    else:
+        choice3 = input("Which door do yo want to go in. Blue, red or yellow").lower
+        if choice3 == "red":
+            print("You went to the dragon room")
+        elif choice3 == "blue":
+            print("You went to the beast room")
+        else:
+            print("Hurrah you win")
+        
